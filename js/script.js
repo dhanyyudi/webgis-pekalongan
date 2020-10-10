5a1ace8212ba31d60bcc0ab5d87b4779335cdf17
Dhany Yudi Prasetyo <67413978+dhanyyudi@users.noreply.github.com>
2020-10-11 05:10:43 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,147 @@
+// Layer Group Marking
+var place = L.layerGroup();
+
+// Monumen Juang 3 Oktober 
+var marker = L.marker([-6.887523, 109.668117])
+    .addTo(place)
+    .bindPopup("<h1>Monumen Juang 3 Oktober</h1> <p>Monumen ini didirikan karena Peristiwa 3 Oktober 1945 merupakan perang untuk mengusir Jepang dari Kota Pekalongan. Peristiwa ini terjadi setelah Indonesia merdeka, yaitu tahun 1945</p> <img src='img/monumen_3_oktober.jpg' height='150px' width='300px'>");
+
+// Tugu Mylpaal
+var marker = L.marker([-6.878964, 109.674562])
+    .addTo(place)
+    .bindPopup("<h1>Tugu Mylpaal</h1> <p>Bangunan bersejarah Tugu Mylpaal berada di selatan Lapngan Jetayu. Tugu ini terdapat penanda titik tengahnya pulau jawa yang dibuat oleh Deandels, Myl atau Milj berarti satuan panjang 1 mil, sedangkan paal berarti tiang. Jadi Tugu Mylpaal ini dalam kata lain adalah titik nol kilometer dari Kota Pekalongan</p> <img src='img/Tugu-Mylpaal-Pekalongan.png' height='300px' width='300px'>");
+
+// Klenteng Poo An Thian 
+var marker = L.marker([-6.879701, 109.674453])
+    .addTo(place)
+    .bindPopup("<h1>Klenteng Poo An Thian</h1> <p>Bangunan ini merupakan tempat ibadah umat Budha di Pekalongan. Lokasinya berada di kawasan Kampung Pecinan. Setiap perayaan tahun baru imlek suasana disini akan ramai dengan masyarakat melaksanakan pawai Gi Aing atau mengarak dewa-dewa dan dengan tarian barongsai.</p> <img src='img/Klenteng-Po-An-Thian-600x400.jpg' height='200px' width='300px'>");
+
+//Jembatan Loji
+var marker = L.marker([-6.879737, 109.675708])
+    .addTo(place)
+    .bindPopup("<h1>Jembatan Loji</h1> <p>Jembatan Loji atau Brug Lodge merupakan salah satu bangunan cagar budaya di Kota Pekalongan yang dibangun pada masa pemerintah kolonial Hindia Belanda. Jembatan ini dahulu sebagai penghubung antara pelabuhan dan pusat kota.</p> <img src='img/jembatan_loji.jpg' height='200px' width='300px'>");
+
+// Kawasan Kampung Batik Jlamprang
+var marker = L.marker([-6.882072, 109.679672])
+    .addTo(place)
+    .bindPopup("<h1>Kawasan Kampung Batik Jlamprang</h1> <p>Batik jlamprang sendiri merupakan motif khas dari batik pekalongan yang menyajikan motif geometris dengan corak warna yang cerah. Tugu ini dibangun oleh PUPR Kota Pekalongan sekaligus sebagai batas di sisi timur.</p> <img src='img/Kampung-Batik-Jlamprang-Krapyak-Pekalongan.jpg' height='300px' width='300px'>");
+
+// Museum Batik
+var marker = L.marker([-6.878574, 109.675484])
+    .addTo(place)
+    .bindPopup("<h1>Museum Batik</h1> <p>Gedung ini tepat berada di timur Lapangan Jetayu, terlihat mencolok dengan tulisan 'Museum Batik'. ini merupakan satu-satunya museum yang ada di Pekalongan, menampilkan koleksi batik dari seluruh Nusantara mulai dari kuno hingga modern. Nampak dari arsitektur gedung yang khas zaman kolonial dan masih kokoh hingag saat ini.</p> <img src='img/Museum-Batik-Pekalongan.jpg' height='200px' width='300px'>");
+
+// Landmark Batik
+var marker = L.marker([-6.878569, 109.674816])
+    .addTo(place)
+    .bindPopup("<h1>Landmark Batik</h1> <p>Kawasan alun-alun utara Kota Pekalongan tepatnya di Jalan Jetayu sudah dikenal sebagai tempat kesenian dan kebudayaan sejak zaman kolonial. Di tempat tersebut terdapat bangunan tua seperti, bekas kantor pusat industri gula zaman penjajahan Belanda. Di kawasan yang sudah ditata dengan Landmark BATIK, taman bermain, Lapangan untuk kegiatan pameran atau event budaya, bangunan-bangunan cagar budaya dan bangunan ibadah yang berada di sekitar kawasan Jetayu.</p> <img src='img/landmark_batik.jpg' height='200px' width='300px'>");
+
+// Kantor Pos Indonesia 
+var marker = L.marker([-6.877753, 109.674103])
+    .addTo(place)
+    .bindPopup("<h1>Kantor Pos Indonesia</h1> <p>Terletak di utara landmark batik, terdapat gedung kantor pos pekalongan yang sudah tua dari zaman Belanda yang fungsinya tetap sama yakni menguru hal surat-menyurat. Yang unik dari gedung ini adalah terdapat Jam dinding yang terpampang di depan memberi kesan yang nyentrik dan kuno</p> <img src='img/Kantor-Pos-Kota-Pekalongan.jpg' height='200px' width='300px'>");
+
+// Gedung Batik TV
+var marker = L.marker([-6.878158, 109.675669])
+    .addTo(place)
+    .bindPopup("<h1>Gedung Batik TV</h1> <p>Awalnya digunakan sebagai kantor dari sebuah badan yang mengurusi masalah pendidikan dari Tk hingga setingkat SMP (Kindergarten, HIS, MULO) pada masa kolonial. Dan seiring berjalannya waktu kini gedung ini digunakan sebagai kantor penyiaran TV Lokal sejak tahun 2012</p> <img src='img/Gedung-Batik-tv-Pekalongan.jpg' height='200px' width='300px'>");
+
+// Pabrik Teh Bandulan
+var marker = L.marker([-6.897785, 109.682776])
+    .addTo(place)
+    .bindPopup("<h1>Pabrik Teh Bandulan</h1> <p>Selain terkenal dengan batiknya, Kota Pekalongan juga memproduksi teh rutin dan banyak pabrik teh yang berdiri di sini. Beberapa memproduksi teh bubuk, hingga kemasan siap saji. Pabrik ini sudah berdiri sejak tahun 1933</p> <img src='img/Teh_Bandulan.jpg' height='300px' width='300px'>");
+
+// Mangrove Park
+var marker = L.marker([-6.858291, 109.675057])
+    .addTo(place)
+    .bindPopup("<h1>Mangrove Park</h1> <p>Kota Pekalongan sebagai Kota Pesisir tak hanya mempunyai pesona wisata pantai nya saja. Melainkan mempunyai Taman Mangrove yang cukup besar. Sebagai habitat unik mempunyai nilai tinggi, keberadaan hutan mangrove dinilai sangat penting karena menjadi dasar pembentukan rantai makanan ekosistem laut, serta mencegah abrasi pantai. Ekowisata Mangrove ini menjadi salah satu upaya dalam rangka mengajak masyarakat untuk ikut serta menjaga kelestarian lingkungan.</p><img src='img/Mangrove-Park-Pekalongan.jpg' height='200px' width='300px'>");
+
+// Pantai Slamaran
+var marker = L.marker([-6.862056, 109.701769])
+    .addTo(place)
+    .bindPopup("<h1>Pantai Slamaran</h1> <p>Pantai Slamaran Indah adalah salah satu Pantai yang diminati oleh masyaratkat di Kota Pekalongan, Jawa Tengah. Pantai terletak dalam kawasan Pelabuhan Perikanan Nusantara Pekalongan, tepatnya di Pekalongan utara. Di pantai ini banyak sekali mitos yang diceritakan bahkan banyak dikenal cukup menyeramkan.</p> <img src='img/pantai_slamaran.jpg' height='200px' width='300px'>");
+
+
+// Layer Group 
+var topomap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+        maxZoom: 17,
+        attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    }),
+
+    hotosm = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap Indonesia</a>'
+    }),
+
+    streets = L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+
+// Definisi Basemap
+var map = L.map('mapid', {
+    center: [-6.874673, 109.678294],
+    zoom: 14,
+    layers: [topomap, streets, hotosm]
+});
+
+// Definisi Baselayer
+var baseLayers = {
+    "TopoMap": topomap,
+    "Streets": streets,
+    "Hot OSM": hotosm
+};
+
+// Definisi Overlay Marking
+var overlays = {
+    "Tempat Bersejarah": place
+};
+
+// Menampilkan Semua Map
+L.control.layers(baseLayers, overlays).addTo(map);
+
+L.latlngGraticule({
+    showLabel: true,
+    zoomInterval: [{
+            start: 2,
+            end: 3,
+            interval: 30
+        },
+        {
+            start: 4,
+            end: 4,
+            interval: 10
+        },
+        {
+            start: 5,
+            end: 7,
+            interval: 5
+        },
+        {
+            start: 8,
+            end: 8,
+            interval: 1
+        },
+        {
+            start: 9,
+            end: 9,
+            interval: 0.5
+        },
+        {
+            start: 10,
+            end: 13,
+            interval: 0.25
+        },
+    ]
+}).addTo(map);
+
+L.control.scale({
+    position: 'bottomleft',
+    maxWidth: 100,
+    metric: true,
+    imperial: true,
+    updateWhenIdle: true
+}).addTo(map);
+L.control.mousePosition().addTo(map);
+L.control.locate().addTo(map);
+setBounds();
